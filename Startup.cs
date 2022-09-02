30aeff047fbbabede29cd3ae1dabe241f4d08faa
HiroyukiWindows <nevertoolatetostartsomething@gmail.com>
2022-09-02 15:01:37 +0900

    スキャフォールディング直後に自動でファイルが作成された直後

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -26,6 +27,9 @@ namespace MvcMovie
             services.AddControllersWithViews();
             // Razorページを有効化
             services.AddRazorPages();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
